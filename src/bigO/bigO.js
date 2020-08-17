f6c15f0a6e031eb0a8157ed69ede62625fad75fe
vedhagiri <giri@brewengage.com>
2020-08-17 10:25:05 +0530

    constant rumtime for big o notation.

---
+++ src/bigO/bigO.js
@@ -0,0 +1,9 @@
+// constant runtime
+// Big O Notation : O(1)
+function log(arr){
+    console.log('---- Big O Constant Runtime -----');
+    console.log(arr[0]);
+    console.log(arr[1]);
+    console.log('---- End -----');
+}
+log([2,4,5,6]);
