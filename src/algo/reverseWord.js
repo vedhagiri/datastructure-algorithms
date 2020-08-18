1941178d26c24b6d14bc7980c1b1f900114d7ded
vedhagiri <giri@brewengage.com>
2020-08-18 14:00:46 +0530

    reverse words algo

---
+++ src/algo/reverseWord.js
@@ -0,0 +1,14 @@
+
+function reverseWords(str){
+    var words = str.split(' ');
+    let result = [];
+
+    words.forEach((word) => {
+        result.push(word.split('').reverse().join(''));
+    });
+    return result.join(' ');
+}
+
+console.log(reverseWords('abc xyz'));
+
+console.log(reverseWords('Coding JavaScript'));
